e44450ca004c782483e7b2e424d14fc6c8f6b424
Nilcire <alun5236@gmail.com>
2024-07-26 22:10:51 +0800

    feat: implement favorite feature

---
+++ app/Models/FavoriteModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FavoriteModel extends Model
+{
+    use HasFactory;
+    protected $table = 'favorites';
+
+    protected $primaryKey = 'id'; // 主鍵
+
+    protected $fillable = [
+        'user_uid',
+        'store_uid',
+    ];
+
+    protected $casts = [
+        // 'rating' => 'decimal:2'
+    ];
+
+    // token
+    public function favorite($user_uid, $store_uid)
+    {
+        return $this->create(
+            [
+                'user_uid' => $user_uid,
+                'store_uid' => $store_uid
+            ]
+        );
+    }
+
+    // token
+    public function unfavorite($user_uid, $store_uid)
+    {
+        return $this->where('user_uid', $user_uid)->where('store_uid', $store_uid)->delete();
+    }
+
+    // token
+    public function get_favorites($user_uid)
+    {
+        $data = \DB::table('favorites') 
+            ->join('stores', 'favorites.store_uid', '=', 'stores.uuid')
+            ->where('user_uid', $user_uid)
+            ->where('is_deleted', false)
+            ->where('is_banned', false)
+            ->select(['uuid', 'is_open', 'name', 'description', 'type', 'banner_image_path']) 
+            ->get();
+
+        return $data;
+    }
+}
